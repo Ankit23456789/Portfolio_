ca14c0bc582d19667fb545017b33fd7205f35b0c
Ankit yadav <ankityadav281998@gmail.com>
2024-02-28 13:36:50 +0530

    first commit

--- JawaScript/Script.js
+++ JawaScript/Script.js
@@ -14,3 +14,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.
 AOS.init({
      disable:'mobile',
 });
+
+
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
